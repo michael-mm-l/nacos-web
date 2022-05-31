b5ec9d9af248bd2552e599c81b6e2f817b9fbb43
michael <113@163.com>
2022-05-31 14:23:52 +0800

    简化项目

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+
+use actix_web::{get, App, HttpServer, Responder};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    format!("{:?}","ddd")
+}
+
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| 
+        App::new()
+        .service(index)
+    
+    )
+    .bind("127.0.0.1:9980")?
+    .run()
+    .await
+}
+
+
+
